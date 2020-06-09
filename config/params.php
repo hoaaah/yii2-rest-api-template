75a233916a3f6ee7c72c04cf3873efabe9e98faf
Heru Arief Wijaya <hoaaah.arief@gmail.com>
2020-06-09 16:46:13 +0700

    Add params template

--- config/params.php
+++ config/params.php
@@ -1,4 +1,15 @@
 <?php
 
 // Created By @hoaaah * Copyright (c) 2020 belajararief.com
-return [];
+return [
+    'useHttpBasicAuth' => true,
+    'useHttpBearerAuth' => true,
+    'useQueryParamAuth' => true,
+
+    /**
+     * use rate limiter for user
+     * you must modified your UserIdentity class, follow this guidelines for complete guide
+     * https://www.yiiframework.com/doc/guide/2.0/en/rest-rate-limiting
+     */
+    'useRateLimiter' => false,
+];
